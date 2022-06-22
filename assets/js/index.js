ec01f17259898bfaf746fb164767845a141c512e
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-22 21:21:09 +0100

    render question from array

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,9 +7,38 @@ const bannerSection = document.getElementById("banner");
 // target main section
 const mainSection = document.getElementById("main");
 
+// current question index
+let questionIndex = 0;
+
+// questions and choices
+const questions = [
+  {
+    question: "Do you like the Mandalorian series?",
+    choices: ["Yes", "No", "Maybe"],
+  },
+  {
+    question: "Looking forward to season 3?",
+    choices: ["Yes", "No", "Maybe"],
+  },
+  {
+    question: "Is Grogu coming back for S3?",
+    choices: ["Yes", "No", "Maybe"],
+  },
+  {
+    question: "Do you think the Darksaber is cool?",
+    choices: ["Yes", "No", "Maybe"],
+  },
+  {
+    question: "Do you want Grogu to become a Mandalorian or a Jedi?",
+    choices: ["Mandalorian", "Jedi", "idk"],
+  },
+];
+
 // function to render question
 const renderQuestion = () => {
-  console.log("render q");
+  // get current question
+  const currentQuestion = questions[questionIndex];
+
   // create section
   const section = document.createElement("section");
   section.setAttribute("class", "content-section question-container");
@@ -17,7 +46,7 @@ const renderQuestion = () => {
   // create h2
   const h2 = document.createElement("h2");
   h2.setAttribute("class", "content-section-title");
-  h2.textContent = "Do you like the Mandalorian series?";
+  h2.textContent = `${questionIndex + 1}. ${currentQuestion.question}`;
 
   // create ul and append 3 li
   const ul = document.createElement("ul");
@@ -26,15 +55,15 @@ const renderQuestion = () => {
   // loop over options to create and append li to ul
   const li1 = document.createElement("li");
   li1.setAttribute("class", "list-item");
-  li1.textContent = "Yes";
+  li1.textContent = currentQuestion.choices[0];
 
   const li2 = document.createElement("li");
   li2.setAttribute("class", "list-item");
-  li2.textContent = "No";
+  li2.textContent = currentQuestion.choices[1];
 
   const li3 = document.createElement("li");
   li3.setAttribute("class", "list-item");
-  li3.textContent = "Maybe";
+  li3.textContent = currentQuestion.choices[2];
 
   ul.append(li1, li2, li3);
 
