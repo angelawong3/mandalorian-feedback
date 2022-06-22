8fc00fcdb53ba9f336931132022e2f36b3bd17ad
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-22 20:59:06 +0100

    dynamically render questions on start

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,9 +4,45 @@ const startButton = document.getElementById("start-btn");
 // target banner section
 const bannerSection = document.getElementById("banner");
 
+// target main section
+const mainSection = document.getElementById("main");
+
 // function to render question
 const renderQuestion = () => {
   console.log("render q");
+  // create section
+  const section = document.createElement("section");
+  section.setAttribute("class", "content-section question-container");
+
+  // create h2
+  const h2 = document.createElement("h2");
+  h2.setAttribute("class", "content-section-title");
+  h2.textContent = "Do you like the Mandalorian series?";
+
+  // create ul and append 3 li
+  const ul = document.createElement("ul");
+  ul.setAttribute("class", "feedback-list");
+
+  // loop over options to create and append li to ul
+  const li1 = document.createElement("li");
+  li1.setAttribute("class", "list-item");
+  li1.textContent = "Yes";
+
+  const li2 = document.createElement("li");
+  li2.setAttribute("class", "list-item");
+  li2.textContent = "No";
+
+  const li3 = document.createElement("li");
+  li3.setAttribute("class", "list-item");
+  li3.textContent = "Maybe";
+
+  ul.append(li1, li2, li3);
+
+  // append h2 and ul to section
+  section.append(h2, ul);
+
+  // append question section to main
+  mainSection.append(section);
 };
 
 // function to remove banner
