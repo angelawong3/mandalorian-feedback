99abc5d37d2847bdb3b64fe93b4bd10bb0cf439a
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-23 01:17:11 +0100

    style for responses page

--- assets/js/responses.js
+++ assets/js/responses.js
@@ -9,40 +9,45 @@ const allResults = JSON.parse(localStorage.getItem("allResults"));
 if (allResults !== null) {
   for (let i = 0; i < allResults.length; i++) {
     const createUl = document.createElement("ul");
-    createUl.setAttribute("id", "userFullName");
+    createUl.setAttribute("class", "userFullName");
     createUl.textContent = "Answered by: " + allResults[i].userFullName;
 
     const li1 = document.createElement("li");
-    li1.setAttribute("id", "questionAndAnswer");
+    li1.setAttribute("class", "questionAndAnswer");
     li1.textContent =
+      "1. " +
       allResults[i].feedbackResults[0].question +
       " " +
       allResults[i].feedbackResults[0].value;
 
     const li2 = document.createElement("li");
-    li2.setAttribute("id", "questionAndAnswer");
+    li2.setAttribute("class", "questionAndAnswer");
     li2.textContent =
+      "2. " +
       allResults[i].feedbackResults[1].question +
       " " +
       allResults[i].feedbackResults[1].value;
 
     const li3 = document.createElement("li");
-    li3.setAttribute("id", "questionAndAnswer");
+    li3.setAttribute("class", "questionAndAnswer");
     li3.textContent =
+      "3. " +
       allResults[i].feedbackResults[2].question +
       " " +
       allResults[i].feedbackResults[2].value;
 
     const li4 = document.createElement("li");
-    li4.setAttribute("id", "questionAndAnswer");
+    li4.setAttribute("class", "questionAndAnswer");
     li4.textContent =
+      "4. " +
       allResults[i].feedbackResults[3].question +
       " " +
       allResults[i].feedbackResults[3].value;
 
     const li5 = document.createElement("li");
-    li5.setAttribute("id", "questionAndAnswer");
+    li5.setAttribute("class", "questionAndAnswer");
     li5.textContent =
+      "5. " +
       allResults[i].feedbackResults[4].question +
       " " +
       allResults[i].feedbackResults[4].value;
